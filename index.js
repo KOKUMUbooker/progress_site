d2e564934f6c568431bcbb20bc3c48943e4878db
cmwema <calebmwema65@gmail.com>
2022-06-11 16:05:26 +0300

    created cards

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let number = document.getElementById("number");
 let counter = 0;
 setInterval(() => {
-  if (counter == 65) {
+  if (counter == 80) {
     clearInterval();
   } else {
     counter += 1;
